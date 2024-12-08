627e8b0a7c002c2f57487949f515104f7b8d0e0c
Jekov-Evgen <jena1245481te@gmail.com>
2024-12-08 18:15:04 +0200

    add sort and print Python

--- sorting.py
+++ sorting.py
@@ -1,6 +1,15 @@
 PATH = "build\Data.txt"
 data_list = []
+result = []
 
 
 with open(PATH, 'r') as f:
     data_list = f.readlines()
+
+for data in data_list:
+    if data.find("not") != -1:
+        continue
+    else:
+        result.append(data)
+        
+print(result)
